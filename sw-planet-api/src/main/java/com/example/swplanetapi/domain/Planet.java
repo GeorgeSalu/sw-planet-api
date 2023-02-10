df1155c7fb80b0e81c346b9e6ae5150df268e8d3
GeorgeSalu <george.salu10@gmail.com>
2023-02-10 13:47:05 -0300

    teste de unidade: cadastro de usuaio com sucesso

--- sw-planet-api/src/main/java/com/example/swplanetapi/domain/Planet.java
+++ sw-planet-api/src/main/java/com/example/swplanetapi/domain/Planet.java
@@ -6,6 +6,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
 @Entity
 @Table(name = "planets")
 public class Planet {
@@ -16,6 +18,12 @@ public class Planet {
 	private String name;
 	private String climate;
 	private String terrain;
+
+	public Planet(String name, String climate, String terrain) {
+		this.name = name;
+		this.climate = climate;
+		this.terrain = terrain;
+	}
 
 	public Long getId() {
 		return id;
@@ -47,6 +55,11 @@ public class Planet {
 
 	public void setTerrain(String terrain) {
 		this.terrain = terrain;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(obj, this);
 	}
 
 }

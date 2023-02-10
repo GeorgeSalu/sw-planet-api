e67d457e50a0093d7a990d44d423c1a18f8e3d8e
GeorgeSalu <george.salu10@gmail.com>
2023-02-10 14:06:53 -0300

    removendo imports nao utilizados

--- sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
+++ sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
@@ -1,14 +1,13 @@
 package com.example.swplanetapi.domain;
 
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
 import static com.example.swplanetapi.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest(classes = PlanetService.class)
 public class PlanetServiceTest {
@@ -18,7 +17,7 @@ public class PlanetServiceTest {
 
 	@MockBean
 	private PlanetRepository planetRepository;
-	
+
 	@Test
 	public void createPlanet_WithValidaData_ReturnsPlanet() {
 		when(planetRepository.save(PLANET)).thenReturn(PLANET);

6c02e032ea9db7d448f61e757dc1c0a250e0c837
GeorgeSalu <george.salu10@gmail.com>
2023-02-10 14:30:18 -0300

    utilizando mockito para teste de unidade

--- sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
+++ sw-planet-api/src/test/java/com/example/swplanetapi/domain/PlanetServiceTest.java
@@ -5,17 +5,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
 
-	@Autowired
+	@InjectMocks
 	private PlanetService planetService;
 
-	@MockBean
+	@Mock
 	private PlanetRepository planetRepository;
 
 	@Test

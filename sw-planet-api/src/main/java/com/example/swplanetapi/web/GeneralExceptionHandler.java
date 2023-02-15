13b598514e6c08914b788d4e0cf93ab772ac5a26
GeorgeSalu <george.salu10@gmail.com>
2023-02-15 14:39:46 -0300

    testando o cenario de planeta existente no controller

--- sw-planet-api/src/main/java/com/example/swplanetapi/web/GeneralExceptionHandler.java
+++ sw-planet-api/src/main/java/com/example/swplanetapi/web/GeneralExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.example.swplanetapi.web;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -16,5 +18,11 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
 		return super.handleMethodArgumentNotValid(ex, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	private ResponseEntity<Object> handleConflict(DataIntegrityViolationException ex) {
+		return ResponseEntity.status(HttpStatus.CONFLICT)
+				.body(ex.getMessage());
 	}
 }

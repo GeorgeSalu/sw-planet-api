04e1d47990f56f2af18b9812e1a153bf8cdb5481
GeorgeSalu <george.salu10@gmail.com>
2023-02-20 10:21:28 -0300

    testes subcotaneos - configuração do banco mysql: profile de teste

--- sw-planet-api/src/test/java/com/example/swplanetapi/PlanetIT.java
+++ sw-planet-api/src/test/java/com/example/swplanetapi/PlanetIT.java
@@ -3,7 +3,9 @@ package com.example.swplanetapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("it")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PlanetIT {
 	

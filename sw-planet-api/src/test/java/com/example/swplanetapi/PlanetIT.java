15e9ba20458b8620b91be01411eba630abacc90e
GeorgeSalu <george.salu10@gmail.com>
2023-02-20 10:44:13 -0300

    testando o cadstro de usuario com sucesso

--- sw-planet-api/src/test/java/com/example/swplanetapi/PlanetIT.java
+++ sw-planet-api/src/test/java/com/example/swplanetapi/PlanetIT.java
@@ -1,16 +1,34 @@
 package com.example.swplanetapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+
+import com.example.swplanetapi.domain.Planet;
+
+import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.*;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PlanetIT {
 	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
 	@Test
-	public void contextLoads() {
+	public void createPlanet_ReturnsCreated() {
+		ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
 		
+		assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		assertThat(sut.getBody().getId()).isNotNull();
+		assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+		assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+		assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
 	}
 }
